79d4123d8481e78714a6fd26a8bef9bc45878388
janay <janayby@gmail.com>
2022-12-08 09:31:08 +0200

    remove propsPractice

--- propsPractice/src/components/Joke.jsx
+++
@@ -1,9 +0,0 @@
-export default function Joke(props) {
-  return (
-    <div className="joke--container">
-      {props.setup && <h2 className="setup">{props.setup}</h2>}
-      <h4 className="punchline">{props.punchline}</h4>
-      <hr />
-    </div>
-  )
-}

240c8f10aa290d82594cb6d75236580bb8af3e75
janay <janayby@gmail.com>
2022-12-09 14:44:37 +0200

    move data comunication in to the card component

--- airbnbPageClone/src/Components/Card.jsx
+++ airbnbPageClone/src/Components/Card.jsx
@@ -1,23 +1,23 @@
 export default function Card(props) {
   let badgeText
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.location === "Online"){
+  } else if (props.item.location === "Online"){
     badgeText = "ONLINE"
   }
   return (
     <section className="container--card">
       <div className="card">
         {badgeText && <div className="card--badge">{badgeText}</div>}
-        <img className="card--photo" src={`src/assets/${props.img}`} alt=""/>
+        <img className="card--photo" src={`src/assets/${props.item.coverImg}`} alt=""/>
         <div className="card--container__details">
           <img className="card--star" src="src/assets/star.png" alt=""/>
-          <small className="card--rating">{props.rating}</small>
-          <small className="card--review-count">({props.review}) • </small>
-          <small className="card--review-location">{props.location}</small>
+          <small className="card--rating">{props.item.stats.rating}</small>
+          <small className="card--review-count">({props.item.stats.reviewCount}) • </small>
+          <small className="card--review-location">{props.item.location}</small>
         </div>
-        <h3 className="card--title">{props.title}</h3>
-        <p className="card--details"><span>From ${props.price}</span> / person</p>
+        <h3 className="card--title">{props.item.title}</h3>
+        <p className="card--details"><span>From ${props.item.price}</span> / person</p>
       </div>
     </section>
   )

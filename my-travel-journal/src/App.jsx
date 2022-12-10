e4e2b1262cf4d947c7ae438747ebe58c9b49c295
janay <janayby@gmail.com>
2022-12-10 14:25:31 +0200

    get the data to the card component

--- my-travel-journal/src/App.jsx
+++ my-travel-journal/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './Card'
+import data from './data'
 
 function App() {
+  const cards = data.map(item => {
+    return <Card item={item}/>
+  })
 
   return (
     <div className="App">
-      <Card />
+      {/* <Card /> */}
+      {cards}
     </div>
   )
 }
--- my-travel-journal/src/Card.jsx
+++ my-travel-journal/src/Card.jsx
@@ -1,18 +1,18 @@
-
-
-export default function Card(){
+export default function Card(props){
   return(
     <div className="card">
-      <img className="card--image" src="src/assets/Geirangerfjord.png" alt=""/>
+      <img className="card--image" src={`src/assets/${props.item.imageUrl}`} alt=""/>
       <div className="card--text">
         <div className="card--firstline">
-          
-          <p className="card--location"><img className="card--location-icon" src="src/assets/Location icon.svg" alt="location icon"/>NORWAY</p>
-          <a href="https://maps.google.com">View on Google Maps</a>
+          <p className="card--location">
+            <img className="card--location-icon" src="src/assets/Location icon.svg" alt="location icon"/>
+            {props.item.location}
+          </p>
+          <a href={`${props.item.googleMapsUrl}`}>View on Google Maps</a>
         </div>
-        <h2 className="card-title">Geirangerfjord</h2>
-        <span className="card--date">12 Jan, 2021 - 24 Jan, 2021</span>
-        <p className="card--main-text">The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.</p>
+        <h2 className="card-title">{props.item.title}</h2>
+        <span className="card--date">{props.item.startDate} - {props.item.endDate}</span>
+        <p className="card--main-text">{props.item.description}</p>
       </div>
     </div>
   )

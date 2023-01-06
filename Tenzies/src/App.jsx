98efec51f4d21e7ad417c1f788a4346b64ac873b
janay <janayby@gmail.com>
2023-01-06 18:11:03 +0200

    create end game

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -5,7 +5,17 @@ import {nanoid} from 'nanoid'
 
 function App() {
   const[dice, setDice] = React.useState(allNewDice())
-
+  const [tenzies, setTenzies] = React.useState(false)
+    
+  React.useEffect(() => {
+      const allHeld = dice.every(die => die.isHeld)
+      const firstValue = dice[0].value
+      const allSameValue = dice.every(die => die.value === firstValue)
+      if (allHeld && allSameValue) {
+          setTenzies(true)
+          console.log("You won!", tenzies)
+      }
+  }, [dice])
   function generateNewDie() {
     return {
       value: Math.ceil(Math.random() * 6),

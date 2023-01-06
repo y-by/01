2399be660bf2efc5f8c9ac498bf01c99643d9c16
janay <janayby@gmail.com>
2023-01-06 07:39:53 +0200

    add uniq keys to elements

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Die from './Die'
+import {nanoid} from 'nanoid'
 
 function App() {
   const[dice, setDice] = React.useState(allNewDice())
@@ -10,13 +11,14 @@ function App() {
     for (let i = 0; i < 10; i++) {
       newDice.push({
         value: Math.ceil(Math.random() * 6),
-        isHeld: false
+        isHeld: false,
+        id: nanoid()
       })
     }
     return newDice
   }
 
-  const diceElements = dice.map(die => <Die value={die.value} />)
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
 
   function rollDice() {
     setDice(allNewDice())
